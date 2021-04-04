7c1656deb4302561fc9b58e9a306a3fd4bbbc26a
victor13bars <victor_bars_bars95@mail.ru>
2021-04-04 22:30:09 +0300

    HW9.

--- src/p2-homeworks/h9/Clock.tsx
+++ src/p2-homeworks/h9/Clock.tsx
@@ -1,34 +1,43 @@
 import React, {useState} from 'react'
 import SuperButton from '../h4/common/c2-SuperButton/SuperButton'
+import s from "./HW9.module.css";
 
 function Clock() {
     const [timerId, setTimerId] = useState<number>(0)
-    const [date, setDate] = useState<Date>()
+    const [date, setDate] = useState<Date>(new Date())
     const [show, setShow] = useState<boolean>(false)
 
     const stop = () => {
-        // stop
+        clearInterval(timerId)
     }
     const start = () => {
         stop()
         const id: number = window.setInterval(() => {
-            // setDate
+            setDate(new Date())
         }, 1000)
         setTimerId(id)
     }
 
     const onMouseEnter = () => {
+        setShow(true)
         // show
     }
     const onMouseLeave = () => {
+        setShow(false)
         // close
     }
+    let hours = date.getHours() < 10 ? "0" + date.getHours() : date.getHours()
+    let minutes = date.getMinutes() < 10 ? "0" + date.getMinutes() : date.getMinutes()
+    let seconds = date.getSeconds() < 10 ? "0" + date.getSeconds() : date.getSeconds()
+    const stringTime = hours + " : " + minutes + " : " + seconds  // fix with date
 
-    const stringTime = 'Time' // fix with date
-    const stringDate = 'Date' // fix with date
+    let year = date.getFullYear();
+    let month = date.getMonth() + 1 < 10 ? "0" + (date.getMonth() + 1) : date.getMonth() + 1;
+    let day = date.getDate() < 10 ? "0" + date.getDate() : date.getDate();
+    const stringDate = day + ' . ' + month +' . ' +year // fix with date
 
     return (
-        <div>
+        <div className={s.defaultDiv}>
             <div
                 onMouseEnter={onMouseEnter}
                 onMouseLeave={onMouseLeave}
--- src/p2-homeworks/h9/HW9.tsx
+++ src/p2-homeworks/h9/HW9.tsx
@@ -1,11 +1,11 @@
 import React from 'react'
 import Clock from './Clock'
+import s from "./HW9.module.css";
 
 function HW9() {
     return (
-        <div>
-            <hr/>
-            homeworks 9
+        <div className={s.column}>
+            <h1>homeworks 9</h1>
 
             {/*should work (должно работать)*/}
             <Clock/>
