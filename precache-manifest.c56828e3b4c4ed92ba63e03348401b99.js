faee3b0235810eddebd3ea9a76a11fcde554d640
victor13bars <victor_bars_bars95@mail.ru>
2021-04-04 22:36:00 +0300

    Updates

---
+++ precache-manifest.c56828e3b4c4ed92ba63e03348401b99.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6fafa7d3edce3f2fe0497c7b18ae08be",
+    "url": "/HW_IGNAT/index.html"
+  },
+  {
+    "revision": "ff93f512847431a2d420",
+    "url": "/HW_IGNAT/static/css/main.b8ea3b76.chunk.css"
+  },
+  {
+    "revision": "a6ecf1aa074a53ab8be2",
+    "url": "/HW_IGNAT/static/js/2.b3f26961.chunk.js"
+  },
+  {
+    "revision": "9b318b6fb13190fe82c0677e9264b3c7",
+    "url": "/HW_IGNAT/static/js/2.b3f26961.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ff93f512847431a2d420",
+    "url": "/HW_IGNAT/static/js/main.48e6b551.chunk.js"
+  },
+  {
+    "revision": "8fee77479f982759af97",
+    "url": "/HW_IGNAT/static/js/runtime-main.39c5ee51.js"
+  }
+]);
